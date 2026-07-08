bd36134f79553d01f9eb5667632d77838bda5d80
mani0019 <kurhemanish68@gmail.com>
2026-07-08 11:25:54 +0530

    Update frontend environment configuration

--- frontend/src/enviroment.js
+++ frontend/src/enviroment.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://your-deployed-backend-url.com" :
+    "https://dristimeet.onrender.com" :
     "http://localhost:8080"
 
 export default server;
