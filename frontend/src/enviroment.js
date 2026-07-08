70fe0977458409cf5411c91c184f14039f981f54
mani0019 <kurhemanish68@gmail.com>
2026-07-08 13:27:25 +0530

    env changes

--- frontend/src/enviroment.js
+++ frontend/src/enviroment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://dristimeet.onrender.com" :
+    "https://dristimeet-02.onrender.com" :
     "http://localhost:8080"
 
 export default server;
